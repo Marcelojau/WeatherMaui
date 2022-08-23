deb3885408aeb492a47f58d4c3bdf17ef0394316
Marcelo Gomes do Nascimento <marcelogn2010@hotmail.com>
2022-08-23 00:15:22 -0300

    Update ApiService.cs

--- WeatherApp/Services/ApiService.cs
+++ WeatherApp/Services/ApiService.cs
@@ -8,7 +8,7 @@ namespace WeatherApp.Services
         public static async Task<Root> GetWeather(double latitude, double longitude)
         {
             var httpClient = new HttpClient();
-            var response = await httpClient.GetStringAsync(String.Format("https://api.openweathermap.org/data/2.5/forecast?lat={0}&lon={1}&units=your_api_key", latitude, longitude));
+            var response = await httpClient.GetStringAsync(String.Format("https://api.openweathermap.org/data/2.5/forecast?lat={0}&lon={1}&units=metric&appid=your_api_key", latitude, longitude));
             return JsonConvert.DeserializeObject<Root>(response);
         }
 
